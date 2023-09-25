77959a90f528e9b058449ed8eabc126da525d6d5
unknown <mriogut600@g.educaand.es>
2023-09-25 11:50:34 +0200

    Proyecto Eurovision Finalizado

--- EuroVision/src/main/java/EuroVision_MRG_Launcher/Launcher.java
+++ EuroVision/src/main/java/EuroVision_MRG_Launcher/Launcher.java
@@ -9,96 +9,101 @@ import EuroVision_MRG_Utils.Pais;
 public class Launcher
 {
 	public static void main(String[] args)
-	{
-		List<String> paisesFinalistas = new ArrayList<>();
-		paisesFinalistas.add("Serbia");
-        paisesFinalistas.add("Moldavia");
-        paisesFinalistas.add("Hungría");
-        paisesFinalistas.add("Ucrania");
-        paisesFinalistas.add("Suecia");
-        paisesFinalistas.add("Australia");
-        paisesFinalistas.add("Noruega");
-        paisesFinalistas.add("Dinamarca");
-        paisesFinalistas.add("Eslovenia");
-        paisesFinalistas.add("Holanda");
-        paisesFinalistas.add("Albania");
-        paisesFinalistas.add("República Checa");
-        paisesFinalistas.add("Lituania");
-        paisesFinalistas.add("Israel");
-        paisesFinalistas.add("Estonia");
-        paisesFinalistas.add("Bulgaria");
-        paisesFinalistas.add("Austria");
-        paisesFinalistas.add("Finlandia");
-        paisesFinalistas.add("Irlanda");
-        paisesFinalistas.add("Chipre");
-        paisesFinalistas.add("Alemania");
-        paisesFinalistas.add("Italia");
-        paisesFinalistas.add("Reino Unido");
-        paisesFinalistas.add("Francia");
-        paisesFinalistas.add("España");
-
-		List<Pais> paises = new ArrayList<>();
-		
-		for (String paisNombre : paisesFinalistas)
 		{
-			paises.add(new Pais(paisNombre));
-		}
-		
-		Random rand = new Random();
-		for (Pais pais : paises)
-		{
-			List<String> paisesVotados = new ArrayList<>(paisesFinalistas);
+			List<String> paisesFinalistas = new ArrayList<>();
+			paisesFinalistas.add("Serbia");
+			paisesFinalistas.add("Moldavia");
+			paisesFinalistas.add("Hungría");
+			paisesFinalistas.add("Ucrania");
+			paisesFinalistas.add("Suecia");
+			paisesFinalistas.add("Australia");
+			paisesFinalistas.add("Noruega");
+			paisesFinalistas.add("Dinamarca");
+			paisesFinalistas.add("Eslovenia");
+			paisesFinalistas.add("Holanda");
+			paisesFinalistas.add("Albania");
+			paisesFinalistas.add("República Checa");
+			paisesFinalistas.add("Lituania");
+			paisesFinalistas.add("Israel");
+			paisesFinalistas.add("Estonia");
+			paisesFinalistas.add("Bulgaria");
+			paisesFinalistas.add("Austria");
+			paisesFinalistas.add("Finlandia");
+			paisesFinalistas.add("Irlanda");
+			paisesFinalistas.add("Chipre");
+			paisesFinalistas.add("Alemania");
+			paisesFinalistas.add("Italia");
+			paisesFinalistas.add("Reino Unido");
+			paisesFinalistas.add("Francia");
+			paisesFinalistas.add("España");
 
-			for (String paisVotado : paisesVotados)
-			{
-				if (!paisVotado.equals(pais.getNombre()))
+			List<Pais> paises = new ArrayList<>();
+
+			for (String paisNombre : paisesFinalistas)
 				{
-					int puntos = rand.nextInt(8) + 1;
-					pais.votar(paisVotado, puntos);
+					paises.add(new Pais(paisNombre));
 				}
-			}
-		}
-		System.out.println("_______PAISES________");
-		for (Pais pais : paises)
-		{
-			System.out.println(pais.getNombre() + ": " + pais.getPuntosTotales() + " puntos");
-		}
 
-		int maxPuntos = -1;
-		int minPuntos = Integer.MAX_VALUE;
-		List<Pais> paisesMax = new ArrayList<>();
-		List<Pais> paisesMin = new ArrayList<>();
+			Random rand = new Random();
+			for (Pais pais : paises)
+				{
+					List<String> paisesVotados = new ArrayList<>(paisesFinalistas);
 
-		for (Pais pais : paises) {
-		    int puntos = pais.getPuntosTotales();
-		    
-		    if (puntos > maxPuntos) {
-		        maxPuntos = puntos;
-		        paisesMax.clear();
-		        paisesMax.add(pais);
-		    } else if (puntos == maxPuntos) {
-		        paisesMax.add(pais);
-		    }
+					for (String paisVotado : paisesVotados)
+						{
+							if (!paisVotado.equals(pais.getNombre()))
+								{
+									int puntos = rand.nextInt(8) + 1;
+									pais.votar(paisVotado, puntos);
+								}
+						}
+				}
+			System.out.println("_______PAISES________");
+			for (Pais pais : paises)
+				{
+					System.out.println(pais.getNombre() + ": " + pais.getPuntosTotales() + " puntos");
+				}
 
-		    if (puntos < minPuntos) {
-		        minPuntos = puntos;
-		        paisesMin.clear();
-		        paisesMin.add(pais);
-		    } else if (puntos == minPuntos) {
-		        paisesMin.add(pais);
-		    }
+			int maxPuntos = -1;
+			int minPuntos = Integer.MAX_VALUE;
+			List<Pais> paisesMax = new ArrayList<>();
+			List<Pais> paisesMin = new ArrayList<>();
+
+			for (Pais pais : paises)
+				{
+					int puntos = pais.getPuntosTotales();
+
+					if (puntos > maxPuntos)
+						{
+							maxPuntos = puntos;
+							paisesMax.clear();
+							paisesMax.add(pais);
+						} else if (puntos == maxPuntos)
+						{
+							paisesMax.add(pais);
+						}
+
+					if (puntos < minPuntos)
+						{
+							minPuntos = puntos;
+							paisesMin.clear();
+							paisesMin.add(pais);
+						} else if (puntos == minPuntos)
+						{
+							paisesMin.add(pais);
+						}
+				}
+
+			System.out.print("\nPaís con la puntuación más alta: ");
+			for (Pais pais : paisesMax)
+				{
+					System.out.println(pais.getNombre());
+				}
+
+			System.out.print("\nPaís con la puntuación más baja: ");
+			for (Pais pais : paisesMin)
+				{
+					System.out.println(pais.getNombre());
+				}
 		}
-
-		System.out.print("\nPaís con la puntuación más alta: ");
-		for (Pais pais : paisesMax)
-		{
-			System.out.println(pais.getNombre());
-		}
-
-		System.out.print("\nPaís con la puntuación más baja: ");
-		for (Pais pais : paisesMin)
-		{
-			System.out.println(pais.getNombre());
-		}
-	}
 }
--- EuroVision/src/main/java/EuroVision_MRG_Utils/Pais.java
+++ EuroVision/src/main/java/EuroVision_MRG_Utils/Pais.java
@@ -7,34 +7,41 @@ import java.util.TreeMap;
 public class Pais
 {
 	private String nombre;
-    private Map<String, Integer> votos;
+	private Map<String, Integer> votos;
 
-    public Pais(String nombre) {
-        this.nombre = nombre;
-        this.votos = new TreeMap<>();
-    }
+	public Pais(String nombre)
+		{
+			this.nombre = nombre;
+			this.votos = new TreeMap<>();
+		}
 
-    public String getNombre() {
-        return nombre;
-    }
+	public String getNombre()
+		{
+			return nombre;
+		}
 
-    public void votar(String pais, int puntos) {
-        votos.put(pais, puntos);
-    }
+	public void votar(String pais, int puntos)
+		{
+			votos.put(pais, puntos);
+		}
 
-    public int getPuntosTotales() {
-    	int suma = 0;
-    	for (Integer voto : votos.values()) {
-    	    suma += voto;
-    	}
-    	return suma;
-    }
+	public int getPuntosTotales()
+		{
+			int suma = 0;
+			for (Integer voto : votos.values())
+				{
+					suma += voto;
+				}
+			return suma;
+		}
 
-    public int getMaxPuntos() {
-        return Collections.max(votos.values());
-    }
+	public int getMaxPuntos()
+		{
+			return Collections.max(votos.values());
+		}
 
-    public int getMinPuntos() {
-        return Collections.min(votos.values());
-    }
+	public int getMinPuntos()
+		{
+			return Collections.min(votos.values());
+		}
 }
